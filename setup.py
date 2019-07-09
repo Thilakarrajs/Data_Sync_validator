fff3323686651fe3a23fcd42ecb96329f0038a46
Thilakarrajs <thilakarraj.s@justintegrateit.com>
2019-07-09 11:48:04 +0530

    project initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+ 
+# Package meta-data.
+NAME = 'data_validator'
+DESCRIPTION = 'Compare database values'
+URL = 'https://github.com/Thilakarrajs/Data_Sync_validator'
+EMAIL = 'thilakarraj.s@justintegrateit.com'
+AUTHOR = 'Thilakar Raj Suyambu'
+REQUIRES_PYTHON = '>=3.7.3'
+VERSION = '0.1.0'
